bb653b56159ecc86b9a1738c67a40a48fc163041
fatemeh <fatemfarjudi@gmail.com>
2025-11-03 11:12:46 +0330

    implement responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-scroll'; 
 
  import logo from "../../assets/logo.png";
+ import menu_icon from "../../assets/menu_icon.png"; 
 import './Navbar.css';
 
 
@@ -14,21 +15,28 @@ const Navbar = () => {
         })
          },[]);
 
+         const[mobileMenu,setMobileMenu] = useState(false);
+         const toggleMenu = () => {
+            mobileMenu? setMobileMenu(false): setMobileMenu(true); 
+         
+         };
 
 
     return (
     
         <nav className= {`container ${sticky ? 'dark-nav':''}`}>
             <img src={logo} alt="Logo" className="logo" />
-            <ul>
+            <ul className={mobileMenu ? 'show-mobile-menu': 'hide-mobile-menu'}>
                 <li> <Link to='hero' smooth={true} offset={0}  duration={500}>Home</Link> </li>
                 <li> <Link to='programs' smooth={true} offset={-260}  duration={500}>Program</Link> </li>
                 <li> <Link to='about' smooth={true} offset={-150}  duration={500}>About Us</Link> </li>
                 <li> <Link to='cafe' smooth={true} offset={-260}  duration={500}>café environment</Link> </li>
                 <li> <Link to='contact' smooth={true} offset={-260}  duration={500}> <button className="btn">Contact Us</button></Link></li>
             </ul>
+            <img src={menu_icon} alt="" className="menu-icon"  onClick={toggleMenu}/>
         </nav>
     );
 };
 
 export default Navbar;
+
